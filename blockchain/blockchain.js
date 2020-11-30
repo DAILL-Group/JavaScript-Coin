907a7cdc05919247833d62eb4a1e62f16c3da1f2
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-11-30 12:22:42 +0100

    Updated blockchain.js

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,5 +1,13 @@
 var blockV1 = require("../blocks/blockV1.js");
-var config = require("../configs/blockchain.json");
+var blockchainConfig = require("../configs/blockchain.json");
+     var defaultBlockTransactions = blockchainConfig.defaultBlockTransactions;
+     var defaultBlockTime = blockchainConfig.defaultBlockTime;
+     var defaultPoaBlockReward = blockchainConfig.defaultPoaBlockReward;
+     var defaultPosBlockReward = blockchainConfig.defaultPosBlockReward;
+     var defaultPowBlockReward = blockchainConfig.defaultPowBlockReward;
+     var defaultMaxCoins = blockchainConfig.defaultMaxCoins;
+     var defaultMaxPools = blockchainConfig.defaultMaxPools;
+     var defaultMaxWallets = blockchainConfig.defaultMaxWallets;
 
 
 var blockchain = {
