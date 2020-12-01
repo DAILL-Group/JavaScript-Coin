2e2cd553c525bc71ec109bf15934f069b40a0046
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-12-01 10:36:23 +0100

    Created jsca.js

---
+++ jsca/jsca.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+
+var datasToEncode = require("../datas/datasToEncode.txt");
+var datasToDecode = require("../datas/datasToDecode.txt");
+
+
+function jsca() {
+     
+     
+     
+};
