b455306c5844717cb2fe0e1b3cf33e57e7e61f50
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-12-02 11:28:42 +0100

    Updated jsca.js

--- jsca/jsca.js
+++ jsca/jsca.js
@@ -5,6 +5,45 @@ var datasToEncode = require("../datas/datasToEncode.txt");
 var datasToDecode = require("../datas/datasToDecode.txt");
 
 
+// variables and functions
+
+var n1 = "a";
+var n2 = "b";
+var n3 = "c";
+var n4 = "d";
+var n5 = "e";
+var n6 = "f";
+var n7 = "g";
+var n8 = "h";
+var n9 = "i";
+var n10 = "j";
+var n11 = "k";
+var n12 = "l";
+var n13 = "m";
+var n14 = "n";
+var n15 = "o";
+var n16 = "p";
+var n17 = "q";
+var n18 = "r";
+var n19 = "s";
+var n20 = "t";
+var n21 = "u";
+var n22 = "v";
+var n23 = "w";
+var n24 = "x";
+var n25 = "y";
+var n26 = "z";
+var n27 = 0;
+var n28 = 1;
+var n29 = 2;
+var n30 = 3;
+var n31 = 4;
+var n32 = 5;
+var n33 = 6;
+var n34 = 7;
+var n35 = 8;
+var n36 = 9;
+
 function jsca() {
      
      
